d53a238cc5f4502882d6e53e9ee912eb45cdce1a
Bidon Pomoev <yoba123@yandex.ru>
2013-09-28 22:27:17 +0400

    login

--- forms.py
+++ forms.py
@@ -1,9 +1,12 @@
 ﻿# coding=utf-8
 from flask.ext.wtf import Form
-from wtforms import TextField, TextAreaField, FileField, HiddenField, BooleanField, SelectField, SubmitField
+from wtforms import TextField, PasswordField, TextAreaField, FileField, HiddenField, BooleanField, SelectField, SubmitField
 from flask.ext.wtf.recaptcha import RecaptchaField
 from wtforms.validators import Required, Length
 
-#class SampleForm(Form):
-#	text = TextField('text', validators=[Required()])
-#	captcha = RecaptchaField()
+class LoginForm(Form):
+    login = TextField('login', validators=[Required()])
+    password = PasswordField('password', validators=[Required()])
+    remember_me = BooleanField('remember-me')
+    #captcha = RecaptchaField()
+    commit = SubmitField('commit')
--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 # coding=utf-8
-from flask import Flask, url_for, render_template, redirect
+from flask import Flask, url_for, render_template, redirect, session, request
 from flask.ext.assets import Environment, Bundle
+from db import *
+from forms import LoginForm
 
 app = Flask(__name__)
 app.config.from_object('config')
 assets_env = Environment(app)
+
+@app.before_request
+def before_request():
+    'This function called before any request'
+
+def redirect_url(default='index'):
+    return request.args.get('next') or \
+           request.referrer or \
+           url_for(default)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if session.get('logged_as'):
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Example
+        session['logged_as'] = form.login.data
+        session.permanent = form.remember_me.data
+        return redirect(url_for('index'))
+
+    return render_template('login-form.html', form=form)
 
 @app.route('/')
 @app.route('/index')
