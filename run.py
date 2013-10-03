55445cc11cd1c603574219a78564ab6e6ee14404
Bidon Pomoev <yoba123@yandex.ru>
2013-10-03 23:54:55 +0400

    no message

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ DEBUG_PORT = 5000
 
 if __name__ == '__main__':
     import sys, main
-    if sys.argv[1] == '-debug':
+    if '-debug' in sys.argv:
         main.app.run(port=DEBUG_PORT, debug=True, host=DEBUG_HOST)
     else:
         main.app.run(port=PORT, debug=False, host=HOST)
